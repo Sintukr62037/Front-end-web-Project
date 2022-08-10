61cd4c743d2d43d7cc2be5a7dfd5c7e5560b8b65
SINTU KUMAR <107838693+Sintukr62037@users.noreply.github.com>
2022-08-10 21:30:41 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>Har Ghar Tiranga</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Notes.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>Azadi Ka Amrit Mahotsav</h1>
+      <p>
+        The Tri-colour flag is a symbol of pride for every Indian. It represents
+        national integrity and signifies the hopes and aspirations of the Indian
+        people.
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
